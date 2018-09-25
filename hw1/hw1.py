638eb85273e2c48518c82c308aa484ff2ae04ca9
Kevin <b03902086@ntu.edu.tw>
2018-09-25 22:15:34 +0800

    hw1 update

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,11 +1,14 @@
 from PIL import Image, ImageDraw, ImageFont
-from scipy import ndimage
+import cv2
 
 # This photo is taken by myself
-image = Image.open('./input.png').convert('L')
-image.show()
+image = cv2.imread('./input.png')
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow('image', image)
 
-sobel = Image.fromarray(ndimage.sobel(image))
+sobelx = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=5)
+sobely = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=5)
+sobel = Image.fromarray((sobelx + sobely)/2).convert('L')
 
 draw = ImageDraw.Draw(sobel)
 # https://github.com/todylu/monaco.ttf
